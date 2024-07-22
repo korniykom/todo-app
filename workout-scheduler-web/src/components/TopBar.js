52fcc1ea76168fc55ed57375fe69248e2b2b0396
Maksym Korniiko <korniykom@gmail.com>
2024-07-22 16:35:43 +0300

    feat: add TopBar to main page

---
+++ workout-scheduler-web/src/components/TopBar.js
@@ -0,0 +1,66 @@
+import "./TopBar.css";
+import { useState } from "react";
+export default function TopBar() {
+  const daysOfTheWeek = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thurday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
+  const months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  const currentDate = new Date();
+  const [difficulty, setDifficulty] = useState("easy");
+
+  return (
+    <div className="top-bar">
+      <div className="top-bar-left">
+        <p>Difficulty: </p>
+        <select
+          className={
+            difficulty === "easy"
+              ? "bg-green"
+              : difficulty === "medium"
+              ? "bg-blue"
+              : difficulty === "hard"
+              ? "bg-pink"
+              : ""
+          }
+          value={difficulty}
+          onChange={(e) => {
+            console.log(e.target.value);
+            setDifficulty(e.target.value);
+          }}
+        >
+          <option value="easy">Easy</option>
+          <option value="medium">Medium</option>
+          <option value="hard">Hard</option>
+          {/* TODO Extreme */}
+        </select>
+      </div>
+      <div className="top-bar-right">
+        <span>
+          {" "}
+          {`${daysOfTheWeek[currentDate.getDay()]} ${currentDate.getDate()} ${
+            months[currentDate.getMonth()]
+          }  ${currentDate.getFullYear()}`}
+        </span>
+      </div>
+    </div>
+  );
+}
