4daf785018b311be4b345e93360e6ed4a3143c24
Maksym Korniiko <korniykom@gmail.com>
2024-07-23 00:30:02 +0300

    feat: add images for cards

--- workout-scheduler-web/src/components/CardList.js
+++ workout-scheduler-web/src/components/CardList.js
@@ -4,13 +4,10 @@ import "./CardList.css";
 export default function CardList() {
   return (
     <div className="container">
-      <DayCard color="#63e6be" />
-      <DayCard color="#e64980" />
-      <DayCard color="#22b8cf" />
-      <DayCard color="#63e6be" />
-      <DayCard color="#e64980" />
-      <DayCard color="#63e6be" />
-      <DayCard color="#22b8cf" />
+      <DayCard color="#63e6be" mussleGroup="chest" />
+      <DayCard color="#e64980" mussleGroup="arms" />
+      <DayCard color="#22b8cf" mussleGroup="abs" />
+      <DayCard color="#63e6be" mussleGroup="legs" />
     </div>
   );
 }
--- workout-scheduler-web/src/components/DayCard.js
+++ workout-scheduler-web/src/components/DayCard.js
@@ -1,11 +1,24 @@
 import "./DayCard.css";
-import img from "../img/Untitled.png";
-export default function DayCard({ color = "yellow" }) {
+import chest from "../img/chest.jpg";
+import arms from "../img/arms.jpg";
+import abs from "../img/abs.jpg";
+import legs from "../img/legs.jpg";
+export default function DayCard({ color = "yellow", mussleGroup = "none" }) {
   const cardStyle = {
     boxShadow: `0 0 4px 2px ${color}`,
     border: `0.3rem solid ${color}`,
     backgroundImage: `linear-gradient(rgba(79, 79, 79, 0.5), rgba(0, 0, 0, 0.5)),
-    url(${img})`,
+    url(${
+      mussleGroup === "chest"
+        ? chest
+        : mussleGroup === "arms"
+        ? arms
+        : mussleGroup === "abs"
+        ? abs
+        : mussleGroup === "legs"
+        ? legs
+        : ""
+    })`,
   };
   const headerStyle = {
     color,
@@ -13,7 +26,9 @@ export default function DayCard({ color = "yellow" }) {
 
   return (
     <div className="conteiner">
-      <h2 style={headerStyle}>Abs day</h2>
+      <h2 style={headerStyle}>
+        {mussleGroup[0].toUpperCase() + mussleGroup.slice(1)} day
+      </h2>
       <div className="day-card" style={cardStyle}></div>
     </div>
   );
