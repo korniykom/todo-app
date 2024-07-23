c20c5014ff4b27a8a8db2a5b3bab0645efab6404
Maksym Korniiko <korniykom@gmail.com>
2024-07-23 18:54:02 +0300

    feat: add new background to main screen and top bar

--- workout-scheduler-web/src/App.js
+++ workout-scheduler-web/src/App.js
@@ -1,11 +1,12 @@
 import CardList from "./components/CardList";
-import TopBar from "./components/TopBar";
+import TopBar from "./Main Screen/Top Bar/TopBar";
+import "./App.css";
 
 export default function App() {
   return (
-    <>
+    <div className="app">
       <TopBar />
       <CardList />
-    </>
+    </div>
   );
 }
